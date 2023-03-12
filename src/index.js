7c8224d834a1e43187ec96623f038703ed20161a
nekaneloop <gonzaleslop03@gmail.com>
2023-03-12 12:19:57 -0500

    Agregando Proyecto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import validator from './validator.js';
+import validator from "./validator.js";
+// todo o que tiene que ver con el DOM
+const btnEnviar = document.querySelector("#btnEnviar");
+const creditCard = document.getElementById("creditCard");
 
-console.log(validator);
+btnEnviar.addEventListener("click", function () {
+  //console.log("click");
+  const numero = creditCard.value;
+  const numeroOculto = validator.maskify(numero);
+  const resultado = validator.isValid(numero);
+  //console.log();
+
+  const mensajeElement = document.querySelector("#mensaje");
+  mensajeElement.innerHTML = resultado
+    ? "Este númeroes válido"
+    : "Este número NO es válido";
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,40 @@
 const validator = {
-  // ...
+  isValid: function (valor) {
+    let resultado = 0;
+    const splitText = [...valor];
+    const revertText = splitText.reverse();
+    // eslint-disable-next-line no-console
+    console.log("revert: ", revertText);
+
+    for (let i = 0; i < revertText.length; i++) {
+      let digito = parseInt(revertText[i]);
+      if (i % 2 !== 0) {
+        digito *= 2;
+        if (digito > 9) {
+          digito -= 9;
+        }
+      }
+      resultado += digito;
+    }
+    // eslint-disable-next-line no-console
+    console.log(resultado);
+    return resultado % 10 === 0;
+  },
+
+  maskify: function (creditCardNumber) {
+    const convirtiendo = creditCardNumber.split("");
+    let result = ''; 
+    for (let i = 0; i < creditCardNumber.length - 4; i++) {
+      convirtiendo[i] = "#";
+    }
+    for (let i = 0; i < convirtiendo.length; i ++) {
+      result = result + convirtiendo[i];
+    }
+    
+    console.log(result);
+   
+    return result 
+  },
 };
 
 export default validator;
